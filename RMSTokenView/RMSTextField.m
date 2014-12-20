80ccd2c3d8e68d6132f56cb26f2f9b0255db717b
Mark Trubnikov <mtrubnikov@gmail.com>
2014-12-20 16:27:48 +0300

    iOS8 deleteBackward bug fixed

--- RMSTokenView/RMSTextField.m
+++ RMSTokenView/RMSTextField.m
@@ -20,4 +20,32 @@
     }
 }
 
+//
+// the following code exists because of the known Apple bug
+// regarding UITextField deleteBackward iOS8 compatibility.
+//
+// For more info see:
+//
+// https://devforums.apple.com/message/1009150#1009150
+// http://stackoverflow.com/a/25862878/956144
+//
+
+- (BOOL)keyboardInputShouldDelete:(UITextField *)textField {
+    BOOL shouldDelete = YES;
+    
+    if ([UITextField instancesRespondToSelector:_cmd]) {
+        BOOL (*keyboardInputShouldDelete)(id, SEL, UITextField *) = (BOOL (*)(id, SEL, UITextField *))[UITextField instanceMethodForSelector:_cmd];
+        
+        if (keyboardInputShouldDelete) {
+            shouldDelete = keyboardInputShouldDelete(self, _cmd, textField);
+        }
+    }
+    
+    if (![textField.text length] && [[[UIDevice currentDevice] systemVersion] intValue] >= 8) {
+        [self deleteBackward];
+    }
+    
+    return shouldDelete;
+}
+
 @end
